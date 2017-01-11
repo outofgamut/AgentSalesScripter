3b776c55f635d3a547cc25f578ac4d4343868a77
outofgamut <jsalvie@gmail.com>
2017-01-11 00:45:25 -0600

    fix module reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { CampaignService } from './shared';
+import { CampaignService } from './campaign.service';
 
 import { AppComponent } from './app.component';
 
